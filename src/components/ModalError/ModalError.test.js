89f26858e756bd1eb0f5bf0f6b4763bdd96f44a9
Florian Bouron <bouron.florian@yahoo.fr>
2018-11-05 23:09:39 +0100

    Add tests for ModalError Component

--- src/components/ModalError/ModalError.test.js
+++ src/components/ModalError/ModalError.test.js
@@ -1,9 +1,36 @@
 import React from "react";
-import { shallow } from "enzyme";
+import { shallow, mount } from "enzyme";
 import ModalError from "./ModalError";
+import { Button, DialogContentText, DialogTitle } from "@material-ui/core";
 
 describe("ModalError Component test", () => {
-  it("renders without crashing", () => {
-    shallow(<ModalError />);
+  describe("Renders without crashing", () => {
+    const error = "";
+    const wrapper = mount(<ModalError error={error} />);
+
+    it("Renders ModalError Component", () => {
+      shallow(<ModalError error={error} />);
+    });
+
+    it("Renders Dialog Title", () => {
+      const dialogTitle = (
+        <DialogTitle id="alert-dialog-title">HTTP connection error</DialogTitle>
+      );
+      expect(wrapper.containsMatchingElement(dialogTitle)).toBeTruthy();
+    });
+
+    it("Renders Dialog Content", () => {
+      const dialogContent = (
+        <DialogContentText id="alert-dialog-description">
+          It seems like an error just occurred: {error}
+        </DialogContentText>
+      );
+      expect(wrapper.containsMatchingElement(dialogContent)).toBeTruthy();
+    });
+
+    it("Renders Button", () => {
+      const dialogTitle = <Button color="primary">Ok</Button>;
+      expect(wrapper.containsMatchingElement(dialogTitle)).toBeTruthy();
+    });
   });
 });
